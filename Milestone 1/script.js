7d920eb8c80ec87fda00b1ede02a174c4ca7b354
Jibran-07 <153424362+Jibran-07@users.noreply.github.com>
2024-09-15 21:10:18 +0500

    Add files via upload

---
+++ Milestone 1/script.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById("btn");
+var skill = document.getElementById("skills");
+btn.addEventListener('click', function () {
+    if (skill.style.display === 'none') {
+        skill.style.display = 'block';
+    }
+    else {
+        skill.style.display = 'none';
+    }
+});
---
+++ Milestone 1/script.ts
@@ -0,0 +1,10 @@
+// const btn = document.getElementById("btn") as HTMLButtonElement
+// const skill = document.getElementById("skills") as HTMLElement
+
+// btn.addEventListener('click', ()=>{
+//     if(skill.style.display === ''){
+//         skill.style.display = 'block'
+//     }else{
+//         skill.style.display = ''
+//     }
+// })
